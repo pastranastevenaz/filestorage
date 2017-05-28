9d6b7696ba65d0241b40f25f3225a87ce8d23b0c
Steven Antonio <pastrana.steven.az@gmail.com>
2017-05-27 20:14:05 -0700

    added resources to login and register routes

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
+
+from api.controllers import auth
 from config import config
 
 def create_app(env):
@@ -8,6 +10,9 @@ def create_app(env):
 
     api_bp = Blueprint('api', __name__)
     api = Api(api_bp)
+
+    api.add_resource(auth.AuthLogin, '/auth/login')
+    api.add_resource(auth.AuthLogin, 'auth/register')
 
     app.register_blueprint(api_bp, url_prefix="api/v1")
 
